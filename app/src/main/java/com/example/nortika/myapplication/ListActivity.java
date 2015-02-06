6edd28d0e6c21678f014d4a05405bc5afcffa5fd
nortika <jong9988>
2015-02-06 16:30:23 +0900

    test

---
+++ app/src/main/java/com/example/nortika/myapplication/ListActivity.java
@@ -0,0 +1,83 @@
+package com.example.nortika.myapplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+public class ListActivity extends Activity {
+    ListView mListView;
+    ArrayAdapter<String> mAdapter;
+    boolean isDelete = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        mListView = (ListView)findViewById(R.id.listView1);
+
+        String[] items = new String[20];
+        for (int i = 0; i < items.length; i++) {
+            items[i] = "Item " + (i + 1);
+        }
+
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, android.R.id.text2,
+                new ArrayList<String>(Arrays.asList(items)));
+        mListView.setAdapter(mAdapter);
+
+        SwipeDismissListViewTouchListener touchListener =
+                new SwipeDismissListViewTouchListener(
+                        mListView,
+                        new SwipeDismissListViewTouchListener.DismissCallbacks() {
+
+                            @Override
+                            public boolean canDismiss(int position) {
+                                return true;
+                            }
+
+                            @Override
+                            public void onDismiss(ListView listView, int[] reverseSortedPositions) {
+                                if(isDelete){
+                                    for (int position : reverseSortedPositions) {
+                                        mAdapter.remove(mAdapter.getItem(position));
+                                    }
+                                    mAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        });
+        mListView.setOnTouchListener(touchListener);
+        mListView.setOnScrollListener(touchListener.makeScrollListener());
+    }
+
+    private void DialogSimple(){
+
+        AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+
+        alt_bld.setMessage("Do you want to add this ?").setCancelable(
+                false).setPositiveButton("Yes",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        isDelete = true;
+                    }
+                }).setNegativeButton("No",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        isDelete = false;
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alt_bld.create();
+        // Title for AlertDialog
+        alert.setTitle("Task delete");
+        // Icon for AlertDialog
+        // alert.setIcon(R.drawable.icon);
+        alert.show();
+    }
+}
